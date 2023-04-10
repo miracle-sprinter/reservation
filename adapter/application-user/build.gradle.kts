02435ce6b066bae510b9100ea9dc90fd8afafda9
BrianDYKim <18k7102dy@naver.com>
2023-04-11 02:07:43 +0900

    add: Jenkinsfile 파이프라인 추가 완료

--- adapter/application-user/build.gradle.kts
+++ adapter/application-user/build.gradle.kts
@@ -1,13 +1,3 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-aop")
 }
-
-tasks {
-    named<Jar>("jar") {
-        enabled = true
-    }
-
-    named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
-        enabled = false
-    }
-}
